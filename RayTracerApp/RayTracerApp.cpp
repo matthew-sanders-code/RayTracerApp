9ac8f4da4d1a8dfdcc520b36d2a9fda6936964a4
getusername67 <61632919+getusername67@users.noreply.github.com>
2025-04-14 20:52:21 -0400

    Add sphere

--- RayTracerApp/RayTracerApp.cpp
+++ RayTracerApp/RayTracerApp.cpp
@@ -3,15 +3,29 @@
 
 #include "color.h"
 #include "vec3.h"
-#include "ray.h";
+#include "ray.h"
 
 #include <iostream>
 using namespace std;
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
-	vec3 unit_direction = unit_vector(r.direction());
-	auto a = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0)
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
 int main()
 {
     // Image
@@ -52,8 +66,7 @@ int main()
 			auto ray_direction = pixel_center - camera_center;
 			ray r(camera_center, ray_direction);
 
-            		color pixel_color = ray_color(r);
-			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            color pixel_color = ray_color(r);
 			write_color(std::cout, pixel_color);
 		}
 	}
